7615087ea78c66871bd62933add133c00dca596e
Pruthvi1-crypto <pruthvigowda2014@gmail.com>
2026-07-08 12:10:15 +0530

    my daily routine

---
+++ my daily routine.py
@@ -0,0 +1,11 @@
+print("My Daily Routine")
+
+print("I wake up at 6 AM.")
+
+print("I go to school at 7 AM.")
+
+print("I play with my friends in the evening.")
+
+print("I study for 1 hour at night.")
+
+print("Total study hours in a week:", 1 * 7)
