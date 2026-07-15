397a753b4434ec0c433c63d9e67190e4f2478545
Pruthvi1-crypto <pruthvigowda2014@gmail.com>
2026-07-15 11:35:55 +0530

    basic program

---
+++ basic.py
@@ -0,0 +1,95 @@
+# print statement:
+
+# print()//function or method
+
+# it is used to display something on the screen
+
+# print("hello")//hello---->argument or parameter
+# print(50)
+
+# print(50 +20)
+
+# print("Age is ", 20)// Age is 20
+
+# print("Hello\nWorld)// Hello
+
+# World
+
+# print("Hello \t World)//Hello World
+#       2. Variables
+
+# it is container that stores information
+
+# variable stors data
+
+# name="Isha"// name stors "Isha"
+
+# age=90
+
+# age// decalre a varaible
+
+# age=90//intialize a varaible
+
+# vari+able
+
+# print(age)//90
+
+# score=50
+
+# print(score)//50
+
+# score=100
+
+# print(score)//100
+# Identifiers
+
+# it is the name we give to a variable , function or classes
+
+# student_name="john"
+
+# //student_name is an identifier but oit combine with value then it will become your variable
+
+# Good Identifier
+
+# age
+
+# student_name
+
+# total_marks
+
+# price
+
+# bad identifiers
+
+# 2age
+
+# student name
+
+# price$
+
+# Rules: 1 contain letter
+
+# 2.Numbers (not first)
+
+# 3.Underscore
+
+# 4. Keywords
+
+# Keywords are special reserved words in python
+
+# we cant use them as a variable name
+
+# ex -->if,else,for,while , True , False, none,Def, class <retir
+
+# start assingment 1
+import keyword
+
+print(keyword.kwlist)
+
+# start assignment 2
+age= 20
+print(age)
+price= 100
+print(price)
+name= "Isha"
+print(name)
