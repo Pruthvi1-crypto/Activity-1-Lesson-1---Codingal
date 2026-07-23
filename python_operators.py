020f43e1093bb12d7c32402a9dae506362b90a65
Pruthvi1-crypto <pruthvigowda2014@gmail.com>
2026-07-23 12:47:32 +0530

    hi

---
+++ python_operators.py
@@ -0,0 +1,17 @@
+a=10
+b=20
+c=30
+d=40
+
+total=a+b+c+d
+print("Total is:",total)
+
+average=total/4
+print("Average is:",average)
+
+#floor division will give me the quotient of the division
+bags=total//4
+print("Number of bags is:",bags)
+
+leftover=total%4
+print("Leftover is:",leftover)
