5f6345a712afcfa24b82f1e4836f98a526ba61ff
Pruthvi1-crypto <pruthvigowda2014@gmail.com>
2026-08-03 12:43:30 +0530

    commiting

---
+++ operators.py
@@ -0,0 +1,15 @@
+age = int(input("Enter your age: "))
+money = int(input("Enter your money: "))
+
+if age < 12 and money >= 100:
+    print("Buy icecream from the shop")
+
+else:
+    print("No icecream")
+
+homework = "no"
+if  not homework ==  "yes":
+    print("Finish your homework first")
+
+else:
+    print("Great job!")
