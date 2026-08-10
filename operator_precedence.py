51cd3647f33c6c8ee9193da2967d572aaf918ea3
Pruthvi1-crypto <pruthvigowda2014@gmail.com>
2026-08-10 12:44:02 +0530

    h

---
+++ operator_precedence.py
@@ -0,0 +1,49 @@
+v = 4
+
+w = 5
+
+x = 8
+
+y = 2
+
+z = 0
+
+z = (v+w) * x / y;
+
+print("Value of (v+w) * x/ y is ", z)
+
+name = "Alex"
+
+age = 0
+
+
+if name == "Alex" or name == "John" and age >= 2 :
+    print("Hello! Welcome.")
+
+else :
+    print("Good Bye!!")
+
+
+numerator = int(input("enter numerator:"))
+denominator = int(input("enter denominator:"))
+if numerator % denominator ==0:
+    print("The numerator is divisible by denominator")
+
+else:
+    print("The number is not divisible by denominator")
+
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter second number:"))
+num3 = int(input("Enter third number:"))
+
+sum = num1 + num2 + num3
+average = sum / 3
+print("The average of three numbers is:", int (average))
+
+
+a = 10
+b = 5
+c = 2
+result = a + b * c
+print("The result of a + b * c is:", result)
