c386d1ada8efab1896384bd88ac46e7b6bee0332
Pruthvi1-crypto <pruthvigowda2014@gmail.com>
2026-08-24 12:39:17 +0530

    h

---
+++ nestedforloop.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    for j in range(4):
+        print("*", end=" ")
+    print()
+
+for i in range(1,6):
+    for j in range(3):
+        print(i, end=" ")
+    print()
+
+#while loop
