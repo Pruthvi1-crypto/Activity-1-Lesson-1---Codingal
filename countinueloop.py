cb590aec8afa0890429c43f2995d03d95ec3237f
Pruthvi1-crypto <pruthvigowda2014@gmail.com>
2026-08-25 11:19:20 +0530

    h

---
+++ countinueloop.py
@@ -0,0 +1,9 @@
+print("🍕🍕🍕🍕🍕welcome to the pizza monster💀👽☠️ restaurant!")
+
+for table in range(1,3):
+    print("table no",table)
+    pizza = 1
+    while pizza <=4:
+        print("pizza no",pizza)
+        pizza +=1
+    print("Table no",table,"is ready to serve")
