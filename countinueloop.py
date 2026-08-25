533a9c96158c2f4ed32e9399cf23052ba4d30ad8
Pruthvi1-crypto <pruthvigowda2014@gmail.com>
2026-08-25 11:35:05 +0530

    h

--- countinueloop.py
+++ countinueloop.py
@@ -4,6 +4,25 @@ for table in range(1,3):
     print("table no",table)
     pizza = 1
     while pizza <=4:
+
+        if pizza == 3:
+            print("pizza is a pineapple pizza🍍🍍")
+            print("monster says yuck skip it!")
+            pizza +=1
+            break
         print("pizza no",pizza)
-        pizza +=1
-    print("Table no",table,"is ready to serve")
+        pizza  +=1
+
+
+#hungry monkey and bannnnannnna
+print("Welcome to the monkey resturant🍌🍌🍌🐒🐵")
+table=1
+
+while table <=2:
+    print("monkey is at table",table)
+
+    bannana=1
+    while bannana <=3:
+        print("monkey eats bannana",bannana)
+        bannana +=1
+    table +=1
